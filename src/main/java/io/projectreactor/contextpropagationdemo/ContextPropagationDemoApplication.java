23e275c9a2b33dd755567a404b27ef687efa9925
Dariusz Jędrzejczyk <jdariusz@vmware.com>
2023-05-08 14:42:04 +0200

    Init

---
+++ src/main/java/io/projectreactor/contextpropagationdemo/ContextPropagationDemoApplication.java
@@ -0,0 +1,13 @@
+package io.projectreactor.contextpropagationdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContextPropagationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContextPropagationDemoApplication.class, args);
+	}
+
+}
